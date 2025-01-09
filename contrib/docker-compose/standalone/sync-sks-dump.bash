ed7814a16e3070d3fe612b6bfeaea84b0d586d6e
Swetha Swaminathan <swetha.swaminathan@canonical.com>
2025-01-09 12:35:37 +0530

    address review comment

--- contrib/docker-compose/standalone/sync-sks-dump.bash
+++ contrib/docker-compose/standalone/sync-sks-dump.bash
@@ -4,4 +4,4 @@ set -eu
 
 docker-compose -f docker-compose.yml -f docker-compose-tools.yml \
     run --rm --entrypoint /bin/sh import-keys \
-         -x -c 'rsync -avr --delete "rsync://rsync.cyberbits.eu/hockeypuck/dump/*.pgp" /import/dump'
+        -x -c 'rsync -avr --delete "rsync://rsync.cyberbits.eu/hockeypuck/dump/*.pgp" /import/dump'
